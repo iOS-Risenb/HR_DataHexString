f1631142c9220a6caa70c80bc04f59afff731cae
obgniyum <muyingbo@risenb.com>
2017-12-21 11:20:58 +0800

    Initial commit

---
+++ Example/HR_DataHexString/XX_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XX_AppDelegate.h
+//  HR_DataHexString
+//
+//  Created by obgniyum on 12/21/2017.
+//  Copyright (c) 2017 obgniyum. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XX_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/HR_DataHexString/XX_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  XX_ViewController.h
+//  HR_DataHexString
+//
+//  Created by obgniyum on 12/21/2017.
+//  Copyright (c) 2017 obgniyum. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XX_ViewController : UIViewController
+
+@end
